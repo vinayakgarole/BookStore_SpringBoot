a2c415e0c2253c47e6e330c80f2c5260fb336362
vinayakgarole <vinayakgarole10@gamil.com>
2023-06-22 12:27:26 +0530

    Implemented book search functionality

---
+++ bookStore/src/test/java/com/bookStore/BookStoreApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
